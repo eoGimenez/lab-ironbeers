d6624f0f8ae790a6801a236a2af9a28d5bd4109e
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-02-08 16:40:49 +0100

    bonus

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const app = express();
 const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
+hbs.registerPartials(path.join(__dirname + "/views/partials"))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -21,5 +22,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beer', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => { 
+    let beersObj = {
+      beers: beersFromApi
+    }
+    res.render('beer', beersObj )
+  })
+  .catch(error => console.log(error));
+})
+app.get('/randombeer', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    let randomObj = {
+      random: responseFromAPI
+    }
+    res.render('random-beer', randomObj);
+  })
+  .catch(error => console.log(error));
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
